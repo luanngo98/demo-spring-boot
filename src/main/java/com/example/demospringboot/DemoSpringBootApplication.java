0b4913caf8cd579c4cbdccb08dd06a93a4674379
LuanNT <luanngo98@gmail.com>
2023-01-16 16:16:38 +0700

    first commit

---
+++ src/main/java/com/example/demospringboot/DemoSpringBootApplication.java
@@ -0,0 +1,22 @@
+package com.example.demospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringBootApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<>("Hello baby", HttpStatus.OK);
+    }
+}
